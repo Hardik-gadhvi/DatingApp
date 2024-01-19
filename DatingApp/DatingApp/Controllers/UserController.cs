c384caad100f4bda713e3b34be5bc8f10a25839e
Hardik-gadhvi <gadhvi.hardik1159@gmail.com>
2024-01-19 11:40:57 +0530

    User controller added

---
+++ DatingApp/DatingApp/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext) {
+
+            _dataContext = dataContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return user;
+        }
+    }
+}
